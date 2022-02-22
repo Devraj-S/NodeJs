c3b520fbdea5c16b3e85561d3274c60a82205972
Devraj Singh <devraj.singh@neosoftmail.com>
2022-02-22 13:44:16 +0530

    cron Job

--- CronScheduler/index.js
+++ CronScheduler/index.js
@@ -1,17 +1,18 @@
 const cron = require('node-cron');
 let shell = require('shelljs');
 
-// * * * * * *
-// | | | | | | 
-// | | | | | +-- Year              (range: 1900-3000)
-// | | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
-// | | | +------ Month of the Year (range: 1-12)
-// | | +-------- Day of the Month  (range: 1-31)
-// | +---------- Hour              (range: 0-23)
-// +------------ Minute            (range: 0-59)
-cron.schedule("* * * * * *", function(){
-    console.log("cron scheduler running...");
-    if(shell.exec("node testFile.js").code !== 0){
+// "* * * * * *"
+//  | | | | | |
+//  | | | | | |
+//  | | | | | day of week (1-7)
+//  | | | | month (1-12)
+//  | | | day of month (1-31)
+//  | | hour (0-23)
+//  | minute (0-59)
+//  second(optional) (0-59)
+cron.schedule("* * * * * *", function () {
+    console.log(new Date().toLocaleString());
+    if (shell.exec("node testFile.js").code !== 0) {
         console.log("Something went wrong.");
     }
 });
